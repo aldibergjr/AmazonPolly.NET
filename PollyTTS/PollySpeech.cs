d7f5476d745151ee7ac428ba372076d9fa0e0704
aldibergjr <aldibergjr@gmail.com>
2019-05-28 09:48:50 -0300

    Exemplo de conversão Básica

---
+++ PollyTTS/PollySpeech.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using System.IO;
+using NAudio.Wave;
+
+namespace PollyTTS
+{
+    class PollySpeech
+    {
+        public static void SynthesizeSpeechMarks(String text, String path)
+        {
+            //Amazon.Runtime.AWSCredentials credential = new Amazon.Runtime.CredentialManagement.SharedCredentialsFile();
+            //Amazon.Runtime.AWSCredentials cr = new Amazon.Runtime.StoredProfileAWSCredentials();
+            
+          
+            
+            
+            AmazonPollyClient pc = new AmazonPollyClient(Amazon.RegionEndpoint.USEast1);
+
+            SynthesizeSpeechRequest sreq = new SynthesizeSpeechRequest();
+            sreq.Text = text;
+            sreq.OutputFormat = OutputFormat.Mp3;
+            sreq.VoiceId = VoiceId.Vitoria;
+            sreq.SampleRate = "8000";
+            SynthesizeSpeechResponse sres = pc.SynthesizeSpeech(sreq);
+
+             using (FileStream fileStream = File.Create(path))
+            {
+                sres.AudioStream.CopyTo(fileStream);
+                fileStream.Flush();
+                fileStream.Close();
+            }
+
+            /*using (var reader = new Mp3FileReader(path))
+                using(var converter = WaveFormatConversionStream.CreatePcmStream(reader))
+            {
+                WaveFileWriter.CreateWaveFile(path, converter);
+            }*/
+
+        }
+    }
+}
---
+++ PollyTTS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollyTTS
+{
+	class Program
+	{
+        public static void Main(string[] args)
+        {
+            PollySpeech.SynthesizeSpeechMarks(@"Olá Maria José..
+Para sua comodidade,, a Universidade Uninassau envia um lembrete no valor de 123 reais e 54 centavos,, que venceu no dia 12 de março de 2019.. Em caso de dúvidas entrar em contato com Universidade Uninassau..clique 1 para pagar.
+clique 2 para contestar.", @"c:\users\Aldiberg\mensagem.mp3");
+        }        
+    }
+}
