9ee58e240cf8bd1b15837db1d7d3d3e8f227de51
aldibergjr <aldibergjr@gmail.com>
2019-05-29 10:20:45 -0300

    Remoção do uso de threads e conversão direta na mesma função criando apenas o arquivo .wav

--- PollyTTS/PollySpeech.cs
+++ PollyTTS/PollySpeech.cs
@@ -7,12 +7,11 @@ using Amazon.Polly;
 using Amazon.Polly.Model;
 using System.IO;
 using NAudio.Wave;
-using System.Threading;
 namespace PollyTTS
 {
     class PollySpeech
     {
-        public void getAudio(String text, String path)
+        async public void getAudio(String text, String path)
         {
             AmazonPollyClient pc = new AmazonPollyClient(Amazon.RegionEndpoint.USEast1);
 
@@ -23,44 +22,35 @@ namespace PollyTTS
             sreq.SampleRate = "8000";
             SynthesizeSpeechResponse sres = pc.SynthesizeSpeech(sreq);
 
-            using (FileStream fileStream = File.Create(path))
-            {
-                sres.AudioStream.CopyTo(fileStream);
-                fileStream.Flush();
-                fileStream.Close();
-            }
-            Thread.Sleep(1000);
-        }
-        public void convertAudio(string path)
-        {
-            
-            using (var reader = new Mp3FileReader(path))
+
+            FileStream fileStream =  File.Create(path);
+
+            sres.AudioStream.CopyTo(fileStream);
+            fileStream.Flush();
+            fileStream.Close();
+            FileStream fs = File.OpenRead(path);
+            using (Mp3FileReader reader = new Mp3FileReader(fs))
             {
                 var newFormat = new WaveFormat(8000, 16, 1);
-                using (var converter = new WaveFormatConversionStream(newFormat, reader)) {
-
-
-                    WaveFileWriter.CreateWaveFile(@"c:\users\Aldiberg\mensagem.wav", converter);
+                using (var converter = new WaveFormatConversionStream(newFormat,reader))
+                {
+                    var pathw = path + ".wav";
+                    WaveFileWriter.CreateWaveFile(pathw, converter);
+                     
                 }
-                   
-                 
-                
             }
-            
-           
+            fs.Dispose();
+            File.Delete(path);
+
 
         }
+     
         public void SynthesizeSpeechMarks(String text, String path)
         {
-            //var func = this.getAudio(text, path);   
-            //Amazon.Runtime.AWSCredentials credential = new Amazon.Runtime.CredentialManagement.SharedCredentialsFile();
-            //Amazon.Runtime.AWSCredentials cr = new Amazon.Runtime.StoredProfileAWSCredentials();
-            // Thread t1 = new Thread(new ThreadStart(this.getAudio(text, path)));
-            Thread audio = new Thread(() => this.getAudio(text,path));
-            Thread convert = new Thread(() => this.convertAudio(@"c:\users\Aldiberg\mensagem.mp3"));
-            audio.Start();
-            audio.Join();
-            convert.Start();
+
+            getAudio(text,path);
+            
+            //convert.Start();
 
 
 
